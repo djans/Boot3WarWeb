6c26aa8dd8d11c1e2dfe7c1ff1cf0fbe1665ef12
JANS Daniel <djans@cogitosum.com>
2024-12-15 15:16:14 -0500

    Ajustement appsec et script ( Using EC2 and not ECS )

--- aws/scripts/application-stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Stop and remove all running containers
-docker ps -q | xargs -r docker stop
-docker ps -aq | xargs -r docker rm
-
-# Optionally, clean up unused Docker resources
-# docker system prune -f
---
+++ aws/scripts/stop_container.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker stop Boot3WarWeb || true
+docker rm Boot3WarWeb || true
