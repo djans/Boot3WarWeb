ffbd47a5a9718f5a3ab2a92d41f376b4f003b3df
JANS Daniel <djans@cogitosum.com>
2025-01-23 08:30:20 -0500

    Update for appspec.zip

--- aws/scripts/pull-ecr-image.sh
+++ aws/scripts/pull-ecr-image.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "Listing files"
+ls -l
 echo "Pulling Docker image from ECR...."
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 227000603860.dkr.ecr.us-east-2.amazonaws.com/cogitosum/boot3warweb
 docker pull 227000603860.dkr.ecr.us-east-2.amazonaws.com/cogitosum/boot3warweb:latest
