3a11e3442a208d0270583538ffd6886103d38eb1
JANS Daniel <djans@cogitosum.com>
2025-02-04 16:10:20 -0500

    Trying again XRay + Lombok

--- src/main/java/com/cogitosum/data/Course.java
+++ src/main/java/com/cogitosum/data/Course.java
@@ -1,33 +1,13 @@
 package com.cogitosum.data;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class Course {
     private String id;
     private int price;
     private String name;
-
-    // Getters and Setters
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
 
